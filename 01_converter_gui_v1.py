fcfa272961bf45f37627e57d3697038c2a7976cd
jack-jm <101677953+jack-jm@users.noreply.github.com>
2024-02-26 23:41:50 +0000

    Continued setting up converter GUI

--- 01_converter_gui_v1.py
+++ 01_converter_gui_v1.py
@@ -5,6 +5,11 @@ class Converter:
 
   def __init__(self):
 
+    # common font for all buttons
+    # Arial, size 14, bold, with white text
+    button_font = ("Arial", "12", "bold")
+    button_fg = "#FFFFFF"
+
     # Set up GUI Frame
     self.temp_frame = Frame(padx=10, pady=10)
     self.temp_frame.grid()
@@ -24,6 +29,34 @@ class Converter:
                                    justify="left")
     self.temp_instructions.grid(row=1)
 
+    self.temp_entry = Entry(self.temp_frame,
+                            font=("Arial", "14")
+                            )
+    self.temp_entry.grid(row=2, padx=10, pady=10)
+
+    error = "Please enter a number"
+    
+    self.temp_error = Label(self.temp_frame, text=error,
+                            fg="#9C0000")
+    self.temp_error.grid(row=3)
+
+    # Conversion, help and history / export buttons
+    self.button_frame = Frame(self.temp_frame)
+    self.button_frame.grid(row=4)
+
+    self.to_celsius_button = Button(self.button_frame,
+                                    text="To Degrees C",
+                                    bg="#990099",
+                                    fg=button_fg,
+                                    font=button_font)
+    self.to_celsius_button.grid(row=0, column=0)
+
+    self.to_fahrenheit_button = Button(self.button_frame,
+                                       text="To Fahrenheit",
+                                       bg="#009900",
+                                       fg=button_fg,
+                                       font=button_font)
+    self.to_fahrenheit_button.grid(row=0, column=1)
 
 # main routine
 if __name__  == "__main__":
